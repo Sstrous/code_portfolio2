14ca68cc25d635b4efcdc575cf28aafe890cd75d
Spike Strous <spike.strous@gmail.com>
2024-03-31 11:43:54 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { config } from 'dotenv';
+config();
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { urlencoded, json } from 'express';
+import * as fs from "fs";
+
+async function bootstrap() {
+	// const httpsOptions = {
+	// 	key: fs.readFileSync('./localhost-key.pem'),
+	// 	cert: fs.readFileSync('./localhost.pem'),
+	// };
+	const app = await NestFactory.create(AppModule, { cors: true });
+	const globalPrefix = 'api';
+
+	const port = process.env.PORT || 3333;
+
+	app.setGlobalPrefix(globalPrefix);
+	app.use(json({ limit: '50mb' }));
+	app.use(urlencoded({ extended: true, limit: '50mb' }));
+	await app.listen(port);
+
+	Logger.log(
+		`🚀 ${process.env.PROJECT_NAME
+		} API is running on: ${await app.getUrl()}`,
+	);
+}
+bootstrap();
